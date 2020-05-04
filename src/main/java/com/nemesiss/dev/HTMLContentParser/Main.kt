f01d0ff91ce1e824683b798385b1f02e64d7f0a8
Nemesiss.Lin <353388639@qq.com>
2020-05-05 01:26:16 +0800

    Add parsing submission works steps

--- src/main/java/com/nemesiss/dev/HTMLContentParser/Main.kt
+++ src/main/java/com/nemesiss/dev/HTMLContentParser/Main.kt
@@ -1,4 +1,5 @@
 package com.nemesiss.dev.HTMLContentParser
+
 import com.alibaba.fastjson.JSONObject
 import com.nemesiss.dev.HTMLContentParser.Model.*
 import org.jsoup.Jsoup
@@ -11,22 +12,23 @@ class Main {
         fun main(args: Array<String>) {
             val parser = ContentParserFactory.Provide()
             val rootRules = JSONObject.parseObject(File("ContentParserRule.json").readText())
-            val Steps = rootRules.getJSONObject("TextContent").getJSONArray("Steps")
-            val elements = Jsoup.parse(File("htmls/text1.html"), "utf-8")
+            val Steps = rootRules.getJSONObject("Submission-IMAGE").getJSONArray("Steps")
+            val elements = Jsoup.parse(File("htmls/submission-image.html"), "utf-8")
 
-            val Result = parser.GoSteps(elements, Steps) as TextContentInfo
-            println(Result.Text)
+            val Result = (parser.GoSteps(elements, Steps) as Array<*>).map { elem -> elem as RecommendItemModelImage }
+            println(Result.size)
             readLine()
         }
 
         @JvmStatic
         fun FindUrl() {
-            val strWithUrl = "ポッピンキャンディ☆フィーバー！(<a href=\"/jump/?url=https%3A%2F%2Fnico.ms%2Fsm35880454\" target=\"_blank\">https://nico.ms/sm35880454</a>)の歌詞になります。"
+            val strWithUrl =
+                "ポッピンキャンディ☆フィーバー！(<a href=\"/jump/?url=https%3A%2F%2Fnico.ms%2Fsm35880454\" target=\"_blank\">https://nico.ms/sm35880454</a>)の歌詞になります。"
             val regex = "<a (?<=(.))href=\"(.*)\" .*>(.*)</a>".toRegex()
             val domain = "https://piapro.jp"
 
             var result = regex.find(strWithUrl)
-            if(result!=null) {
+            if (result != null) {
                 val total = result.groups[0]
 
                 val href = result.groups[2]
@@ -34,11 +36,10 @@ class Main {
 
                 val hrefBegin = total!!.range.first
 
-                val clickableText = strWithUrl.replace(regex,innerText!!.value)
+                val clickableText = strWithUrl.replace(regex, innerText!!.value)
                 println(clickableText)
-                println(clickableText.substring(hrefBegin, hrefBegin+innerText.value.length-1))
-            }
-            else {
+                println(clickableText.substring(hrefBegin, hrefBegin + innerText.value.length - 1))
+            } else {
 
             }
         }
--- src/main/java/com/nemesiss/dev/HTMLContentParser/Steps/ContentParserImpl.kt
+++ src/main/java/com/nemesiss/dev/HTMLContentParser/Steps/ContentParserImpl.kt
@@ -12,7 +12,7 @@ import org.jsoup.select.Elements
 import java.io.File
 
 
-class ContentParserImpl  constructor(
+class ContentParserImpl constructor(
     val singleInSingleOut: Map<String, SingleInputSingleOutputElementStep<out Any>>,
     val singleInMultiOut: Map<String, SingleInputMultiOutputElementStep<Element, Elements>>,
     val multiInMultiOut: Map<String, MultiInputMultiOutputElementStep<Any>>
@@ -25,6 +25,7 @@ class ContentParserImpl  constructor(
             return StepName.matches(Regex("^.*Single$"))
         }
 
+        @JvmStatic
         fun IsMultiInput(StepName: String): Boolean {
             return StepName.matches(Regex("^forEach.*\$"))
         }
@@ -36,12 +37,12 @@ class ContentParserImpl  constructor(
         println(multiInMultiOut.size)
     }
 
-    fun ParseRule(content: JSONObject) : Any {
-        val RequestUrl = content.getString("RequestUrl")
-        val RequestParameters = content.getJSONObject("RequestParameter")
-        val RequestCookie = content.getJSONObject("RequestCookie")
-
-//        println(RequestUrl)
+    fun ParseRule(content: JSONObject): Any {
+//        val RequestUrl = content.getString("RequestUrl")
+//        val RequestParameters = content.getJSONObject("RequestParameter")
+//        val RequestCookie = content.getJSONObject("RequestCookie")
+//
+////        println(RequestUrl)
 
         val Steps = content.getJSONArray("Steps")
         val elements = Jsoup.parse(File("htmls/musicid.html"), "utf-8")
@@ -57,21 +58,25 @@ class ContentParserImpl  constructor(
         val StepCount = steps.size
 
         for (i in 0 until StepCount) {
-            var step = steps.getJSONObject(i)
+            val step = steps.getJSONObject(i)
             CurrentStepName = step.keys.first()
             CurrentStepRule = step.getJSONObject(CurrentStepName)
-            if (IsSingleOutput(CurrentStepName)) {
-                val stepProcessor = singleInSingleOut[CurrentStepName]
-                    ?: throw InvalidStepExecutorException("Error! Empty step processor! $CurrentStepName")
-                LastStepResult = stepProcessor.Process(LastStepResult as Element, CurrentStepRule)
-            } else if (IsMultiInput(CurrentStepName)) {
-                val stepProcessor = multiInMultiOut[CurrentStepName]
-                    ?: throw InvalidStepExecutorException("Error! Empty step processor! $CurrentStepName")
-                LastStepResult = stepProcessor.Process(LastStepResult as Elements, CurrentStepRule)
-            } else {
-                val stepProcessor = singleInMultiOut[CurrentStepName]
-                    ?: throw InvalidStepExecutorException("Error! Empty step processor! $CurrentStepName")
-                LastStepResult = stepProcessor.Process(LastStepResult as Element, CurrentStepRule)
+            LastStepResult = when {
+                IsSingleOutput(CurrentStepName) -> {
+                    val stepProcessor = singleInSingleOut[CurrentStepName]
+                        ?: throw InvalidStepExecutorException("Error! Empty step processor! $CurrentStepName")
+                    stepProcessor.Process(LastStepResult as Element, CurrentStepRule)
+                }
+                IsMultiInput(CurrentStepName) -> {
+                    val stepProcessor = multiInMultiOut[CurrentStepName]
+                        ?: throw InvalidStepExecutorException("Error! Empty step processor! $CurrentStepName")
+                    stepProcessor.Process(LastStepResult as Elements, CurrentStepRule)
+                }
+                else -> {
+                    val stepProcessor = singleInMultiOut[CurrentStepName]
+                        ?: throw InvalidStepExecutorException("Error! Empty step processor! $CurrentStepName")
+                    stepProcessor.Process(LastStepResult as Element, CurrentStepRule)
+                }
             }
         }
         return LastStepResult
